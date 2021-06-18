56e8a419180821906393bc66033ab83b500a1825
thanhnvse <thanhnv.se@gmail.com>
2021-06-19 06:13:59 +0700

    [API] update api

--- src/main/java/com/drowsiness/dto/datatracking/DataTrackingResponseForUserAndDeviceDTO.java
+++ src/main/java/com/drowsiness/dto/datatracking/DataTrackingResponseForUserAndDeviceDTO.java
@@ -1,2 +1,8 @@
-package com.drowsiness.dto.datatracking;public class DataTrackingResponseForUserAndDeviceDTO {
+package com.drowsiness.dto.datatracking;
+
+import lombok.Data;
+
+@Data
+public class DataTrackingResponseForUserAndDeviceDTO {
+
 }
