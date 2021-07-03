a7383333dc8df02bdeea954af41dc6b9d72ffb5a
huyluvnhjm99 <datpro7703@gmail.com>
2021-07-03 17:50:17 +0700

    Update tự tạo date mới khi udate
    Update tự tạo date mới khi udate

--- src/main/java/com/drowsiness/controller/UserDeviceController.java
+++ src/main/java/com/drowsiness/controller/UserDeviceController.java
@@ -131,7 +131,7 @@ public class UserDeviceController {
         if(userDeviceService.checkExistedUser(userDeviceDTO.getUserId(),userDeviceDTO.getDeviceId())){
             UserDevice existedUser = userDeviceService.findByUserIdAndDeviceId(userDeviceDTO.getUserId(),userDeviceDTO.getDeviceId());
             existedUser.setConnected(true);
-            existedUser.setConnectedAt(new Date().getTime());
+            existedUser.setConnectedAt(StaticFuntion.getDate());
             UserDevice updateConnectedUserDevice = userDeviceService.saveUserDevice(existedUser);
             UserDeviceConnectedResponseDTO userDeviceUpdateResponseDTO = modelMapper.map(updateConnectedUserDevice, UserDeviceConnectedResponseDTO.class);
             return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResult<>(userDeviceUpdateResponseDTO,"Your acccount has been connected with device"));
