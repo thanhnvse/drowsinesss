79a2a24838c2822f8aa68b77124e7ce24a1d0d09
huyluvnhjm99 <datpro7703@gmail.com>
2021-08-14 18:04:33 +0700

    00

--- src/main/java/com/drowsiness/DrowsinessApplication.java
+++ src/main/java/com/drowsiness/DrowsinessApplication.java
@@ -3,11 +3,12 @@ package com.drowsiness;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class DrowsinessApplication {
+public class DrowsinessApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DrowsinessApplication.class, args);
